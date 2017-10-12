d22e78b56637a7e4d142a3b4fbcd208b1beb83bd
ad970f <ad970f@naver.com>
2017-10-12 14:25:59 +0900

    update imsi.cs

--- imsi.cs
+++ imsi.cs
@@ -1,20 +1,126 @@
-1. MS EA 계약 담당자 변경
-2. 통합모니터링 시스템 검수 여부
-3. NES-PDF 컴플라이언스 이슈 -> 라이선스 인계
-4. 노트북 1대, MS-Suffice 1대 인계
+
 
 git config --global user.name "Your Name Here"
 git config --global user.email "your_email@youremail.com"
 git help [command]
 git init
 git status
-git add file.ext
+git add file.ext // modified to staged
 git add .
-git commit -m "remarks"
+git commit -m "remarks" // staged to repository
 git remote add origin https://github.com/username/myproject.git 
 git remote -v
-git push origin master 
+git push origin master // local repository to remote repository
 git branch [new branch name] // create new branch
-git checkout [branch name]
+git checkout [branch name] // switch to branch
 git merge [branch name] // merge to master branch
-git pull
+git pull // update local repository of current checkout branch from remote repository
+
+1. 프로그래밍 언어 및 도구
+	1.1 예스트레이더	
+	1.2 예스랭귀지
+		코드 보안
+		표준 코딩 템플릿
+	1.3 예스스팟
+	1.4 Visaul Studio 
+	1.5 C#
+	1.6 MS-SQL
+	1.7 엑셀
+	
+2. 시뮬레이션
+	2.1 기초통계
+		평균
+		분산
+		표준편차
+		평균편차
+		확률밀도함수
+	2.2 주요 성과 측정 지표
+		손익비 (Profit Factor)
+		승률 (Percent Profitable)
+		총거래횟수 (Total Number of Trades)
+		최대손실폭 (Maximum Drawdown)
+		평균손익비 (Ratio Average Winning Trade / Average Losing Trade)
+		연평균수익률 (Annual Rate of Return)
+		보상비율	
+	2.3 타임프레임
+		고정시간
+		가변시간
+	2.4 전략 최적화
+		변수 최적화
+		필터 개발
+	2.5 포트폴리오 최적화
+		Efficient Frontier
+		전략 성과 데이터 수집
+		전략 성과 데이터 구성
+		엑셀 해찾기 (Excel Solver)
+		
+3. 선물 트레이딩 시스템
+	2.1 진입 및 청산 설계 원칙
+	2.2 시스템 카테고리
+	2.3 가격 기반 시스템
+	2.4 호가 기반 시스템
+	2.5 거래 기반 시스템
+	2.6 참조 기반 시스템
+	
+4. 선물 트레이딩 시그널을 이용한 옵션 매매
+	4.1 예스스팟 기반
+	4.2 API 기반	
+	
+5. 매매 기록
+	4.1 기준가 수익률
+	4.2 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+1. MS EA 계약 담당자 변경
+2. 통합모니터링 시스템 검수 여부
+3. NES-PDF 컴플라이언스 이슈 -> 라이선스 인계
+4. 노트북 1대, MS-Suffice 1대 인계
