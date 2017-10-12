b0c90f1f59cdc8a699bf13a0fbd18089f4a71b6c
ad970f <ad970f@naver.com>
2017-10-12 09:15:29 +0900

    Add imsi.cs

---
+++ imsi.cs
@@ -0,0 +1,20 @@
+1. MS EA 계약 담당자 변경
+2. 통합모니터링 시스템 검수 여부
+3. NES-PDF 컴플라이언스 이슈 -> 라이선스 인계
+4. 노트북 1대, MS-Suffice 1대 인계
+
+git config --global user.name "Your Name Here"
+git config --global user.email "your_email@youremail.com"
+git help [command]
+git init
+git status
+git add file.ext
+git add .
+git commit -m "remarks"
+git remote add origin https://github.com/username/myproject.git 
+git remote -v
+git push origin master 
+git branch [new branch name] // create new branch
+git checkout [branch name]
+git merge [branch name] // merge to master branch
+git pull
