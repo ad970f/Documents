406576677bfebf415e2422661cc961303caa9953
ad970f <ad970f@naver.com>
2017-10-19 16:31:42 +0900

    small changes

--- imsi.cs
+++ imsi.cs
@@ -1,4 +1,8 @@
-
+1. MS EA 계약 담당자 변경
+2. 통합모니터링 시스템 검수 여부
+3. NES-PDF 라이선스 증서 인계
+4. 노트북 1대, MS-Suffice 1대 인계
+5. 인천 POE 스위치 및 AP 장비 교체필요 인지
 
 git config --global user.name "Your Name Here"
 git config --global user.email "your_email@youremail.com"
@@ -16,11 +20,13 @@ git checkout [branch name] // switch to branch
 git merge [branch name] // merge to master branch
 git pull // update local repository of current checkout branch from remote repository
 
+
 1. 개발 언어 및 도구
 	1.1 예스트레이더	
 	1.2 예스랭귀지
 		코드 보안
 		표준 코딩 템플릿
+		코딩 권고 사항
 	1.3 예스스팟
 	1.4 Visaul Studio 
 	1.5 C#
@@ -30,18 +36,20 @@ git pull // update local repository of current checkout branch from remote repos
 2. 시뮬레이션
 	2.1 기초통계
 		평균
-		분산
-		표준편차
-		평균편차
-		확률밀도함수
+			산술평균/기하평균/지수이동평균/중앙값
+		산포도
+			평균편차/분산/표준편차
+		확률함수
+			확률밀도함수
 	2.2 주요 성과 측정 지표
-		손익비 (Profit Factor)
-		승률 (Percent Profitable)
-		총거래횟수 (Total Number of Trades)
-		최대손실폭 (Maximum Drawdown)
-		평균손익비 (Ratio Average Winning Trade / Average Losing Trade)
-		연평균수익률 (Annual Rate of Return)
+		손익비 
+		승률 
+		총거래횟수 
+		최대손실폭
+		평균손익비
+		연평균수익률 
 		보상비율	
+		샤프레이쇼
 	2.3 타임프레임
 		고정시간
 		가변시간
@@ -53,10 +61,11 @@ git pull // update local repository of current checkout branch from remote repos
 			리스크 고정 수익률 최대화
 			수익률 고정 리스크 최소화
 			수익률/리스크 비율 최대화
-		Efficient Frontier
-		전략 성과 데이터 수집
-		전략 성과 데이터 구성
-		엑셀 해찾기 (Excel Solver)
+		효율적 투자선
+		엑셀기반 최적화 시뮬레이션
+			전략 성과 데이터 수집
+			전략 성과 데이터 구성
+			엑셀 해찾기 
 	2.6 포트폴리오 리밸런싱
 		시점 기준 리밸런싱
 		임계치 기준 리밸런싱
@@ -65,19 +74,19 @@ git pull // update local repository of current checkout branch from remote repos
 	3.1 개발 방식
 		Top-Down vs. Bottom-Up
 		진입 및 청산 설계 원칙
-	3.2 시스템 유형
+	3.2 시스템 유형 I
 		추세추종
 		역추세
 		비추세
-	3.3 데이터별 시스템
-		가격 기반 시스템
-		호가 기반 시스템
-		거래 기반 시스템
-		참조 기반 시스템
+	3.3 시스템 유형 II
+		가격 기반
+		호가 기반
+		거래량 기반
+		참조 기반
 	3.4 실전 시스템
-		추세추종 (가격 기반)
-		역추세 (호가 기반)
-		비추세 (참조 기반)
+		가격 기반 추세추종  
+		호가 기반 역추세
+		참조 기반 비추세
 
 4. 선물 트레이딩 시그널을 이용한 옵션 매매
 	4.1 예스스팟 기반
@@ -88,56 +97,3 @@ git pull // update local repository of current checkout branch from remote repos
 	4.2 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-1. MS EA 계약 담당자 변경
-2. 통합모니터링 시스템 검수 여부
-3. NES-PDF 컴플라이언스 이슈 -> 라이선스 인계
-4. 노트북 1대, MS-Suffice 1대 인계
